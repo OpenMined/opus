c475c264c4ce466f21e6519b8fb62d0681814281
Plamen Hristov <hristov.reading@gmail.com>
2020-03-20 01:12:17 +0200

    Example route configuration and documentation

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -1,12 +1,39 @@
 import os
+
 from flask import Flask
+
+
+PACKAGE_VERSION = "0.0.0"
+APP_NAME = 'Private Identity Server'
 
 def create_app():
   app = Flask(__name__)
   app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DB_URI')
   app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-  from . import routes, services, models
-  routes.init_app(app)
-  models.init_app(app)
+
+  register_blueprints(app)
+  register_extensions(app)
 
   return app
+
+
+def register_extensions(app):
+  """
+  Register extensions with the Flask application.
+  Order is important!
+  """
+  from .models import db
+  from .extensions import migrate
+  from .spec import configure_spec
+  db.init_app(app)
+  migrate.init_app(app, db)
+  configure_spec(app)
+
+def register_blueprints(app):
+  """register all needed flask blueprints with the current app"""
+
+  from .routes import all_blueprints
+
+  for bp in all_blueprints:
+    app.register_blueprint(bp)
+
---
+++ server/app/spec.py
@@ -0,0 +1,39 @@
+from apispec.ext.marshmallow import MarshmallowPlugin
+from apispec_webframeworks.flask import FlaskPlugin
+from flasgger import Swagger, APISpec, fields
+from flask_marshmallow import Marshmallow
+
+from app.models import User
+
+ma = Marshmallow()
+
+
+class ErrorSchema(ma.Schema):
+    message = fields.Str(required=True)
+
+
+class UserSchema(ma.ModelSchema):
+    class Meta:
+        model = User
+
+
+definitions = [ErrorSchema, UserSchema]
+
+
+def configure_spec(app):
+    ma.init_app(app)
+    app.config['SWAGGER'] = {'uiversion': 3}
+    spec = APISpec(
+        title='Private Identity Server',
+        version='0.0.0',
+        openapi_version='2.0',
+        plugins=[
+            FlaskPlugin(),
+            MarshmallowPlugin(),
+        ],
+    )
+    template = spec.to_flasgger(
+        app,
+        definitions=definitions
+    )
+    Swagger(app, template=template)
