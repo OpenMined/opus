5c25ba0a0f75518e810191dcb5ffc6c0bcce95f9
FarrandTom <33008723+FarrandTom@users.noreply.github.com>
2020-03-20 16:44:54 +0000

    Added tokens

--- server/app/routes/auth.py
+++ server/app/routes/auth.py
@@ -1,7 +1,8 @@
-from flask import Blueprint, Response, redirect, url_for, jsonify, request
+from flask import Blueprint, Response, redirect, url_for, jsonify, request, g
 from flask_login import current_user, login_user
+from flask_httpauth import HTTPBasicAuth
 from app.models import User, db
-from .errors import bad_request
+from .errors import bad_request, error_response
 
 BASE_URL = ''
 LOGIN_VIEW = {'rule': '/login', 'methods': ['POST'], 'endpoint': 'login'}
@@ -9,9 +10,33 @@ SIGNUP_VIEW = {'rule': '/signup', 'methods': ['POST'], 'endpoint': 'signup'}
 
 auth = Blueprint(name='auth', import_name=__name__, url_prefix=BASE_URL)
 
+basic_auth = HTTPBasicAuth()
+
+@basic_auth.verify_password
+def verify_password(email, password):
+    user = User.query.filter_by(email=email).first()
+    if user is None:
+        return False
+    g.current_user = user
+    return user.check_password(password)
+
+@basic_auth.error_handler
+def basic_auth_error():
+    return error_response(401)
+
+@auth.route('/tokens', methods=['POST'])
+@basic_auth.login_required
+def get_token():
+    token = g.current_user.get_token()
+    db.session.commit()
+    return jsonify({'token': token})
+
 # Login route for users to authenticate into
 @auth.route(**LOGIN_VIEW)
 def login():
+  # Think I've done this wrong. Don't need to make use of LoginManager. 
+  # Simply need to return the token back to the front end.
+  # Will implement token authentication and then strip back the unnecessary stuff. 
   data = request.get_json()
   user = User.query.filter_by(email=data['email']).first()
   
