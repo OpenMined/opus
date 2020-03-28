07b98ddf5cfda1f25ec863c4f5165adbeed0f15a
Plamen Hristov <hristov.reading@gmail.com>
2020-03-28 02:33:47 +0200

    Added automatic migration on container start.

--- server/tasks/db.py
+++ server/tasks/db.py
@@ -1,6 +1,9 @@
 import os
+import time
 
 from invoke import task, Collection
+from sqlalchemy import create_engine
+from sqlalchemy.exc import OperationalError
 
 from app import create_app
 
@@ -11,6 +14,28 @@ def get_env(test=False):
     env = 'test' if test else 'development'
     os.environ['FLASK_CONFIGURATION'] = env
     return env
+
+
+@task
+def wait(ctx, max_attempts=3, wait_time=1):
+    attempts = 0
+    app = create_app()
+    with app.app_context():
+        while True:
+            try:
+                e = create_engine(app.config['SQLALCHEMY_DATABASE_URI'])
+                e.connect()
+                break
+            except OperationalError as e:
+                if attempts >= max_attempts:
+                    raise e
+                attempts += 1
+                time.sleep(wait_time)
+                print("Attempting to connect to database.")
+    print("Connection to database established.")
+
+
+db_collection.add_task(wait)
 
 
 @task(help={'message': "Pass a message to the revision"})
